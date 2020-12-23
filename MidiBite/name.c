ae26f21ac71af24c26a42d7d2a59c64fc94b4261
Robin Giese <robin@grumpycorp.com>
2020-12-22 18:41:39 -0800

    Gardening: Make custom device name slightly less worse

--- MidiBite/name.c
+++ MidiBite/name.c
@@ -2,15 +2,7 @@
 
 #include "usb_names.h"
 
-// Edit these lines to create your own name.  The length must
-// match the number of characters in your custom name.
-
-#define MIDI_NAME                          \
-  {                                        \
-    'M', 'i', 'd', 'i', 'B', 'i', 't', 'e' \
-  }
-#define MIDI_NAME_LEN 8
-
-// Do not change this part.  This exact format is required by USB.
-
-struct usb_string_descriptor_struct usb_string_product_name = {2 + MIDI_NAME_LEN * 2, 3, MIDI_NAME};
+struct usb_string_descriptor_struct usb_string_product_name = {
+    (8 + 1) * 2,  // size of L"MidiBite" including terminator as uint16_t wide string
+    3,
+    {'M', 'i', 'd', 'i', 'B', 'i', 't', 'e'}};
